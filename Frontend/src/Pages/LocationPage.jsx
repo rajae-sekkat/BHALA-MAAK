1becdc08ccb107269ae557bfc13e6a339e638a92
rajae-sekkat <sekkatrajae301@gmail.com>
2023-12-29 16:59:53 +0100

    d

--- Frontend/src/Pages/LocationPage.jsx
+++ Frontend/src/Pages/LocationPage.jsx
@@ -1,14 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./LocationPage.css";
 import NavBar from "../Components/NavBar";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { Icon } from "leaflet";
+import customIconUrl from "../images/icon.png"; // Import the image
 
-function LocationPage(){
+function LocationPage() {
+  const [userLocation, setUserLocation] = useState([48.8566, 2.3522]);
 
-    return (
-        <div className="home1">
-            <NavBar></NavBar>
-        </div>
-    )
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setUserLocation([latitude, longitude]);
+        },
+        (error) => {
+          console.error("Error getting user location:", error.message);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by your browser.");
+    }
+  }, []);
+
+  const customIcon = new Icon({
+    iconUrl: customIconUrl,
+    iconSize: [38, 38],
+  });
+
+  const markers = [
+    {
+      geocode: userLocation,
+      popUp: "Hello this is me",
+    },
+  ];
+
+  return (
+    <div>
+      <NavBar />
+      <MapContainer center={userLocation} zoom={13} className="leaflet-container">
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {markers.map((marker, index) => (
+          <Marker position={marker.geocode} icon={customIcon} key={index}>
+            <Popup>{marker.popUp}</Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
+  );
 }
 
 export default LocationPage;
