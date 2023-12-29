2c67c6a59603323f5ddfca4849024660a574166c
rajae-sekkat <sekkatrajae301@gmail.com>
2023-12-29 17:45:54 +0100

    d

--- Frontend/src/Pages/LocationPage.jsx
+++ Frontend/src/Pages/LocationPage.jsx
@@ -7,50 +7,56 @@ import { Icon } from "leaflet";
 import customIconUrl from "../images/icon.png"; // Import the image
 
 function LocationPage() {
-  const [userLocation, setUserLocation] = useState([48.8566, 2.3522]);
+    const [userLocation, setUserLocation] = useState(null);
 
-  useEffect(() => {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          setUserLocation([latitude, longitude]);
-        },
-        (error) => {
-          console.error("Error getting user location:", error.message);
-        }
-      );
-    } else {
-      console.error("Geolocation is not supported by your browser.");
-    }
-  }, []);
+    useEffect(() => {
+      if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+            setUserLocation([latitude, longitude]);
+          },
+          (error) => {
+            console.error("Error getting user location:", error.message);
+          },
+          {
+            enableHighAccuracy: true, // Request high accuracy (if available)
+            timeout: 5000,            // Set a timeout (in milliseconds)
+            maximumAge: 0             // Force a fresh location reading
+          }
+        );
+      } else {
+        console.error("Geolocation is not supported by your browser.");
+      }
+    }, []);
+  
+    useEffect(() => {
+      console.log("userLocation:", userLocation);
+    }, [userLocation]);
 
   const customIcon = new Icon({
     iconUrl: customIconUrl,
     iconSize: [38, 38],
   });
 
-  const markers = [
-    {
-      geocode: userLocation,
-      popUp: "Hello this is me",
-    },
-  ];
 
   return (
-    <div>
-      <NavBar />
+    <div className="location-page">
+        <div className="navbar">
+        <NavBar />
+        </div>
+        <div  className="leaflet-container-wrapper">
+        {userLocation && (
       <MapContainer center={userLocation} zoom={13} className="leaflet-container">
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-
-        {markers.map((marker, index) => (
-          <Marker position={marker.geocode} icon={customIcon} key={index}>
-            <Popup>{marker.popUp}</Popup>
+          <Marker position={userLocation} icon={customIcon} >
+            <Popup>Hello this is me</Popup>
           </Marker>
-        ))}
       </MapContainer>
+        )}
+    </div>
     </div>
   );
 }
